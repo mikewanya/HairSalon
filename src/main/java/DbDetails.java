cf694070c0759ca9d28ebfbd7ade52781fce4fff
mikewanya <wanyamalately@gmail.com>
2019-04-30 20:00:42 +0300

    heroku

---
+++ src/main/java/DbDetails.java
@@ -0,0 +1,15 @@
+
+public class DbDetails {
+
+    private static String username = "wanya";
+    private static String password = "password";
+
+    public static String getUsername(){
+        return username;
+    }
+    public static String getPassword(){
+        return password;
+    }
+
+
+}
